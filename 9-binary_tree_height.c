a72a044d50d693ab711eafeccbb4282e746361a5
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-04-18 09:47:08 +0100

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,6 +17,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	/* Return the height of the tree as the maximum of left and right subtree heights plus 1 */
+	/* Return height of the tree as maximum of left and right subtree heights */
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
